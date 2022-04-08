0ea0eabe21d20b5d9f4e493b5275f04b09087360
Nicolaj Skov Nielsen <55740458+nicni19@users.noreply.github.com>
2022-04-08 08:41:02 +0200

    further work on the lightweight frontend

--- frontend_light/index.js
+++ frontend_light/index.js
@@ -9,6 +9,38 @@ function listTest(){
 }
 
 function clearList(){
-    let ul = document.getElementById('resultsList').innerHTML="";
+    document.getElementById('resultsList').innerHTML="";
     document.getElementById('resultsWrapper').scrollTop = 0;
+}
+
+function newTest(){
+    node = document.createElement('li')
+    node.append('<div>hej</div>')
+    document.getElementById('resultsList').append(node)
+}
+
+function testListFunc(){
+    obj = {
+        0: {
+            "name": "Dabs",
+            "hours":3
+        },
+        1: {
+            "name": "CUbs",
+            "hours":3
+        }
+    }
+    
+    obj.array.forEach(element => {
+        /*
+        document.getElementById("container").append(
+            "<p class='logElement'>" + element.name + "</p>"
+        )
+        */
+        //"<p class='logElement'>" + element.name + "</p>"
+        let node = document.createElement('li')
+        node.append("<p class='logElement'>" + element.name + "</p>")
+        document.getElementById('resultsList').append(document.createElement('li').append("Hej!"))
+        //"<div class='logElement' style={display:flex; justify-content: center; height: 30px; width: 95%; outline: 2px; outline-style: none; margin-bottom: 10px;}><div>" + element.name + "</div><div>" + element.hours + "</div></div>"
+    });
 }
