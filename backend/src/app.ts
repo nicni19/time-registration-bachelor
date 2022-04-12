00507c8ae1a18a9b5c26a359712b9a50f3a04f5c
benji703 <benjadcool@gmail.com>
2022-04-12 12:48:20 +0200

    Add api endpoint for querying on time period

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -61,6 +61,48 @@ app.get('/authTest', async (req, res) => {
   
 });
 
+app.get('/getLogElements/:startDate/:endDate', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  console.log(req.params.startDate);
+  console.log(req.params.endDate);
+  let startDate: Date;
+  let endDate: Date;
+  try {
+    startDate = new Date(req.params.startDate.replace('T', ' '));
+    endDate = new Date(req.params.endDate.replace('T', ' '));
+    console.log(endDate);
+    
+    if (isNaN(startDate.valueOf()) || isNaN(endDate.valueOf())) {
+      throw "Parameters are not correct date strings. Format should be like this: '2022-04-04T00:00:00.0000000'." 
+      + " URL should be /getLogElements/'startDate'/'endDate'";
+    }
+    if (startDate.valueOf() > endDate.valueOf()) {
+      throw "Start date is later than the end date";
+    }
+
+  } catch (error) {
+    res.status(400);
+    res.send({
+      ErrorMessage: error
+    });
+    return;
+  }
+
+  let token: string = req.headers.authorization.split(' ')[1];
+  console.log(token);
+  let requestJSON = JSON.parse(JSON.stringify(req.headers));
+  console.log(requestJSON.userid);
+
+  let logElements: LogElement[];
+
+  await core.graphUpdate(requestJSON.userid as string, token).then( async () => {
+    logElements = await core.getLogElements(requestJSON.userid);
+  });
+  
+  res.status(200);
+  res.send(logElements);
+});
+
 app.get('/getLogElements', async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   
